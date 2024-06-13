1da33448c8b6aec87de928acd5791468059be4c0
Klee <118732983+jfdaiow@users.noreply.github.com>
2024-06-13 23:13:57 +0800

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,34 +1,46 @@
-function uploadImageForFaceExtraction() {
-    var input = document.getElementById('imageInput');
-    var file = input.files[0];
-    var originalImage = document.getElementById('originalImage');
-    var processedImage = document.getElementById('processedImage');
-    var originalTitle = document.getElementById('originalTitle');
-    var processedTitle = document.getElementById('processedTitle');
+function uploadImagesForVerification() {
+    var input1 = document.getElementById('imageInput1');
+    var input2 = document.getElementById('imageInput2');
+    var file1 = input1.files[0];
+    var file2 = input2.files[0];
 
-    // 显示原图
-    var reader = new FileReader();
-    reader.onload = function(e) {
-        originalImage.src = e.target.result;
-        originalImage.style.display = 'block'; // 显示原图
-        originalTitle.style.display = 'block'; // 显示原图标题
+    var originalImage1 = document.getElementById('originalImage1');
+    var originalImage2 = document.getElementById('originalImage2');
+    var originalTitle1 = document.getElementById('originalTitle1');
+    var originalTitle2 = document.getElementById('originalTitle2');
+    var verificationResult = document.getElementById('verificationResult');
+
+    // 显示原图1
+    var reader1 = new FileReader();
+    reader1.onload = function(e) {
+        originalImage1.src = e.target.result;
+        originalImage1.style.display = 'block'; // 显示原图1
+        originalTitle1.style.display = 'block'; // 显示原图1标题
     };
-    reader.readAsDataURL(file);
+    reader1.readAsDataURL(file1);
+
+    // 显示原图2
+    var reader2 = new FileReader();
+    reader2.onload = function(e) {
+        originalImage2.src = e.target.result;
+        originalImage2.style.display = 'block'; // 显示原图2
+        originalTitle2.style.display = 'block'; // 显示原图2标题
+    };
+    reader2.readAsDataURL(file2);
 
     // 使用FormData上传图片
     var formData = new FormData();
-    formData.append('image', file);
+    formData.append('image1', file1);
+    formData.append('image2', file2);
 
-    fetch('/extract_faces', {
+    fetch('/verify', {
         method: 'POST',
         body: formData
     })
-    .then(response => response.blob())
-    .then(blob => {
-        var url = URL.createObjectURL(blob);
-        processedImage.src = url;
-        processedImage.style.display = 'block'; // 显示处理后的图像
-        processedTitle.style.display = 'block'; // 显示处理后图像的标题
+    .then(response => response.json())
+    .then(data => {
+        verificationResult.textContent = 'Verification Result: ' + data.result;
+        verificationResult.style.display = 'block'; // 显示验证结果
     })
     .catch(error => console.error('Error:', error));
 }
